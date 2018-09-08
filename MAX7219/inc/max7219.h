4262db9ffb19aa93240241e536da4ceef20e9972
Mateusz Kiełbasa <sobriodev@gmail.com>
2018-09-08 18:53:25 +0200

    Macros for registers

--- MAX7219/inc/max7219.h
+++ MAX7219/inc/max7219.h
@@ -10,15 +10,24 @@
 
 #include "board.h"
 
+/* ------ Macros ------ */
+
+#define NO_OP_REG                   0x00
+#define DECODE_MODE_REG             0x09
+#define INTENSITY_REG               0x0A
+#define SCAN_LIMIT_REG              0x0B
+#define SHOUTDOWN_REG               0x0C
+#define DISPLAY_TEST_REG            0x0F
+#define DIGIT_REG(DIGIT)            ((DIGIT) + 1)
+
+#define MAX7219_FRAME(ADDR, DATA)   ((uint16_t) (((ADDR) << 8) | (DATA)))
+
+/* ---- End Macros ---- */
+
 /* ------ Private variables and functions ------ */
 /* ---- End private variables end functions ---- */
 
-
 /* ------ Public variables and functions ------ */
-
-void MAX7219InitializeSPI(LPC_SSP_T *ssp);
-
 /* ---- End public variables end functions ---- */
-
 
 #endif /* MAX7219_H_ */
