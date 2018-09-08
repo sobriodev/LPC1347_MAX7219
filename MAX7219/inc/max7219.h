904c968df60355c7c8f315fc4a33665ac459e60e
Mateusz Kiełbasa <sobriodev@gmail.com>
2018-09-08 14:31:16 +0200

    Gitignore files, MAX7219 .h and .c files template

---
+++ MAX7219/inc/max7219.h
@@ -0,0 +1,24 @@
+/*
+ * max7219.h
+ *
+ *  Created on: 8 Sep 2018
+ *      Author: sobriodev
+ */
+
+#ifndef MAX7219_H_
+#define MAX7219_H_
+
+#include "board.h"
+
+/* ------ Private variables and functions ------ */
+/* ---- End private variables end functions ---- */
+
+
+/* ------ Public variables and functions ------ */
+
+void MAX7219InitializeSPI(LPC_SSP_T *ssp);
+
+/* ---- End public variables end functions ---- */
+
+
+#endif /* MAX7219_H_ */
--- lpc_chip_13xx/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /home/sobriodev/Documents/MCUXpresso_10.2.1_795/workspace/lpc_chip_13xx/inc/ring_buffer.h \
- /home/sobriodev/Documents/MCUXpresso_10.2.1_795/workspace/lpc_chip_13xx/inc/lpc_types.h
-
-/home/sobriodev/Documents/MCUXpresso_10.2.1_795/workspace/lpc_chip_13xx/inc/ring_buffer.h:
-
-/home/sobriodev/Documents/MCUXpresso_10.2.1_795/workspace/lpc_chip_13xx/inc/lpc_types.h:
